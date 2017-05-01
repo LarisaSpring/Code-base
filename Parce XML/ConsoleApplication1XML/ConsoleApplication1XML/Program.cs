4b64fd1e6e3f37fd390d920971c451881dbd7428
Larisa Gavriljuk <gavriljukla@gmail.com>
2017-05-01 08:44:27 +0200

    *

---
+++ Parce XML/ConsoleApplication1XML/ConsoleApplication1XML/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApplication1XML
+{
+    public class GenerateXml
+    {
+        private static void Main()
+        {
+            var doc = new XDocument(
+                new XElement("root",
+                    new XElement("someNode", "someValue")
+                    )
+                );
+
+            doc.Save(@"c:\foo.xml");
+
+            Console.WriteLine(doc.ToString());
+            Console.ReadKey();
+
+        }
+    }
+
+}
